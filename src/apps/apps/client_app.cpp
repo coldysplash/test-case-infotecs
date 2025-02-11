f3c34dbd36cc920ef4223ae85919ea02cc89c2da
Ivan <ivanreshetnikov235@gmail.com>
2025-02-11 19:31:23 +0700

    add client_app and server_app in apps. tested

--- src/apps/apps/client_app.cpp
+++ src/apps/apps/client_app.cpp
@@ -1,1 +1,53 @@
-int main() {}
+#include "client/client.hpp"
+#include "common/parser.hpp"
+
+#include <iostream>
+#include <pthread.h>
+#include <string>
+
+void *thread_func_1(void *data) {
+  auto *shared_buffer = static_cast<client::SharedBuffer *>(data);
+  std::cout << "Enter Digits >> ";
+  while (true) {
+    std::string input;
+    std::getline(std::cin, input);
+    if (client::check_text(input)) {
+      parser::modify_str(input);
+      shared_buffer->set_buff(input);
+    } else {
+      continue;
+    }
+  }
+  return nullptr;
+}
+
+void *thread_func_2(void *data) {
+  auto *shared_buffer = static_cast<client::SharedBuffer *>(data);
+  while (true) {
+    const client::Client client_;
+    const std::string buff = shared_buffer->get_buff();
+    shared_buffer->clear();
+    std::cout << "\nModify input data: " << buff << '\n';
+    if (client_.connect_to_server("127.0.0.1", 8080) == 0) {
+      client_.send_to_server(parser::total_sum_of_nums(buff));
+      std::cout << "Sending completed.\n";
+    }
+  }
+  return nullptr;
+}
+
+int main() {
+  client::SharedBuffer shared_buff;
+  pthread_t thread_1;
+  pthread_t thread_2;
+  if (pthread_create(&thread_1, nullptr, thread_func_1, &shared_buff) != 0) {
+    std::cerr << "Error creating thread" << '\n';
+  }
+  if (pthread_create(&thread_2, nullptr, thread_func_2, &shared_buff) != 0) {
+    std::cerr << "Error creating thread" << '\n';
+  }
+  pthread_join(thread_1, nullptr);
+  pthread_join(thread_2, nullptr);
+
+  return 0;
+}
--- src/apps/apps/server_app.cpp
+++ src/apps/apps/server_app.cpp
@@ -1,1 +1,33 @@
-int main() {}
+#include "common/parser.hpp"
+#include "server/server.hpp"
+
+#include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+
+  try {
+    serv::Server server;
+    server.serv_bind();
+    server.serv_listen();
+    int client_socket = 0;
+    std::cout << "Server started!\n";
+    while (true) {
+      client_socket = server.accept_client_connection();
+      const std::string message = server.recv_message(client_socket);
+      if (parser::analyze_message(message)) {
+        std::cout << "Data received successfully! Message - " << message
+                  << '\n';
+      } else {
+        std::cout << "\nError! Received data does not match format.\n";
+      }
+      std::cout << "\nWaiting for a message ...\n";
+    }
+    close(client_socket);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  return 0;
+}
